afa589e6218f4aa6b3b046d4fad5f1764922a4db
RajalakshmiSankaran23 <113708575+RajalakshmiSankaran23@users.noreply.github.com>
2023-12-21 10:28:41 +0530

    Add files via upload

---
+++ CODING CHALLENGE/CODING-CHALLENGE-CARRENTAL.sql
@@ -0,0 +1,288 @@
+create database CarRentalSystem;
+use CarRentalSystem;
+
+CREATE TABLE Vehicle (
+    vehicleID INT PRIMARY KEY,
+    make VARCHAR(255),
+    model VARCHAR(255),
+    year INT,
+    dailyRate DECIMAL(10, 2),
+    status VARCHAR(20) CHECK (status IN ('available', 'notAvailable')),
+    passengerCapacity INT,
+    engineCapacity INT
+);
+
+CREATE TABLE Customer (
+    customerID INT PRIMARY KEY,
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    email VARCHAR(255),
+    phoneNumber VARCHAR(15)
+);
+
+CREATE TABLE Lease (
+    leaseID INT PRIMARY KEY,
+    vehicleID INT,
+    customerID INT,
+    startDate DATE,
+    endDate DATE,
+    type VARCHAR(20) CHECK (type IN ('DailyLease', 'MonthlyLease')),
+    FOREIGN KEY (vehicleID) REFERENCES Vehicle(vehicleID),
+    FOREIGN KEY (customerID) REFERENCES Customer(customerID)
+);
+
+CREATE TABLE Payment (
+    paymentID INT PRIMARY KEY,
+    leaseID INT,
+    paymentDate DATE,
+    amount DECIMAL(10, 2),
+    FOREIGN KEY (leaseID) REFERENCES Lease(leaseID)
+);
+
+insert into Vehicle values (1,' Toyota' ,'Camry' ,2022, 50.00,'available', 4, 2500);
+insert into Vehicle values (2, 'Honda', 'Civic' ,2023, 45.00,'available', 7, 1500);
+insert into Vehicle values(3 , 'Ford', 'Focus', 2022, 48.00, 'notAvailable', 4, 1400);
+insert into Vehicle values(4, 'Nissan' ,'Altima' ,2023, 52.00, 'available', 7, 1200);
+insert into Vehicle values(5, 'Chevrolet', 'Malibu', 2022, 47.00, 'available', 4, 1800);
+insert into Vehicle values(6, 'Hyundai' ,'Sonata ',2023, 49.00, 'notAvailable', 7, 1400);
+insert into Vehicle values(7, 'BMW','3 Series' ,2023, 60.00, 'available', 7, 2499);
+insert into Vehicle values(8, 'Mercedes' ,'C-Class',2022, 58.00, 'available', 8, 2599);
+insert into Vehicle values(9, 'Audi' ,'A4 ',2022 ,55.00, 'notAvailable', 4, 2500);
+insert into Vehicle values(10, 'Lexus' ,'ES', 2023, 54.00, 'available', 4 ,2500);
+
+select * from Vehicle;
+
+insert into Customer values (1 , 'John',' Doe', 'johndoe@example.com', 555-555-5555);
+insert into Customer values (2, 'Jane',' Smith' ,'janesmith@example.com ',555-123-4567);
+insert into Customer values (3 ,'Robert', 'Johnson', 'robert@example.com', 555-789-1234);
+insert into Customer values (4 ,'Sarah', 'Brown', 'sarah@example.com ',555-456-7890);
+insert into Customer values (5 ,'David', 'Lee', 'david@example.com ',555-987-6543);
+insert into Customer values (6 ,'Laura ','Hall ','laura@example.com', 555-234-5678);
+insert into Customer values (7 ,'Michael',' Davis',' michael@example.com ',555-876-5432);
+insert into Customer values (8 ,'Emma', 'Wilson ','emma@example.com ',555-432-1098);
+insert into Customer values (9 ,'William',' Taylor',' william@example.com', 555-321-6547);
+insert into Customer values (10,' Olivia', 'Adams', 'olivia@example.com ',555-765-4321);select * from Customer;INSERT INTO Lease VALUES(1, 1, 1, '2023-01-01', '2023-01-05', 'Daily');
+INSERT INTO Lease VALUES(2, 2, 2, '2023-02-15', '2023-02-28', 'Monthly');
+INSERT INTO Lease VALUES(3, 3, 3, '2023-03-10', '2023-03-15', 'Daily');
+INSERT INTO Lease VALUES(4, 4, 4, '2023-04-20', '2023-04-30', 'Monthly');
+INSERT INTO Lease VALUES(5, 5, 5, '2023-05-05', '2023-05-10', 'Daily');
+INSERT INTO Lease VALUES(6, 4, 3, '2023-06-15', '2023-06-30', 'Monthly');
+INSERT INTO Lease VALUES(7, 7, 7, '2023-07-01', '2023-07-10', 'Daily');
+INSERT INTO Lease VALUES(8, 8, 8, '2023-08-12', '2023-08-15', 'Monthly');
+INSERT INTO Lease VALUES(9, 3, 3, '2023-09-07', '2023-09-10', 'Daily');
+INSERT INTO Lease VALUES(10, 10, 10, '2023-10-10', '2023-10-31', 'Monthly');
+
+select * from Lease;
+
+INSERT INTO Payment VALUES(1, 1, '2023-01-03', 200.00);
+INSERT INTO Payment VALUES(2, 2, '2023-02-20', 1000.00);
+INSERT INTO Payment VALUES(3, 3, '2023-03-12', 75.00);
+INSERT INTO Payment VALUES(4, 4, '2023-04-25', 900.00);
+INSERT INTO Payment VALUES(5, 5, '2023-05-07', 60.00);
+INSERT INTO Payment VALUES(6, 6, '2023-06-18', 1200.00);
+INSERT INTO Payment VALUES(7, 7, '2023-07-03', 40.00);
+INSERT INTO Payment VALUES(8, 8, '2023-08-14', 1100.00);
+INSERT INTO Payment VALUES(9, 9, '2023-09-09', 80.00);
+INSERT INTO Payment VALUES(10, 10, '2023-10-25', 1500.00);
+
+
+---1. Update the daily rate for a Mercedes car to 68.
+
+UPDATE Vehicle
+SET dailyRate = 68.00
+WHERE make = 'Mercedes';
+
+select * from Vehicle;
+
+---2. Delete a specific customer and all associated leases and payments.
+
+DELETE FROM Payment
+WHERE leaseID IN (SELECT leaseID FROM Lease WHERE customerID = (SELECT customerID FROM Customer WHERE firstName = 'John' AND lastName = 'Doe'));
+
+DELETE FROM Lease
+WHERE customerID = (SELECT customerID FROM Customer WHERE firstName = 'John' AND lastName = 'Doe');
+
+DELETE FROM Customer
+WHERE firstName = 'John' AND lastName = 'Doe';
+
+--- 3. Rename the "paymentDate" column in the Payment table to "transactionDate"
+
+table to "transactionDate".
+ALTER TABLE Payment
+RENAME COLUMN paymentDate TO transactionDate;
+
+---4. Find a specific customer by email.
+
+SELECT * FROM Customer WHERE email = 'johndoe@example.com';
+
+---5. Get active leases for a specific customer.
+SELECT 
+    Lease.leaseID,
+    Lease.startDate,
+    Lease.endDate,
+    Lease.type
+FROM 
+    Lease
+JOIN 
+    Customer ON Lease.customerID = Customer.customerID
+WHERE 
+    Customer.email = 'johndoe@example.com'
+    AND CURDATE() BETWEEN Lease.startDate AND Lease.endDate;
+
+
+---6. Find all payments made by a customer with a specific phone number
+
+SELECT *FROM Payment
+WHERE leaseID IN (SELECT leaseID FROM Lease WHERE customerID = (SELECT customerID FROM Customer WHERE phoneNumber = '555-765-4321'));
+---7. Calculate the average daily rate of all available carsSELECT AVG(dailyRate) AS averageDailyRate
+FROM Vehicle
+WHERE status = 'available';
+
+---8. Find the car with the highest daily rate
+
+SELECT *
+FROM Vehicle
+WHERE dailyRate = (SELECT MAX(dailyRate) FROM Vehicle);
+
+---9. Retrieve all cars leased by a specific customer.
+
+SELECT Vehicle.*
+FROM Vehicle
+JOIN Lease ON Vehicle.vehicleID = Lease.vehicleID
+WHERE Lease.customerID = (
+    SELECT customerID
+    FROM Customer
+    WHERE email = 'johndoe@example.com'
+);
+
+---10. Find the details of the most recent lease
+
+SELECT Vehicle.*, Lease.*
+FROM Vehicle
+JOIN Lease ON Vehicle.vehicleID = Lease.vehicleID
+WHERE Lease.startDate = (
+    SELECT MAX(startDate)
+    FROM Lease
+);
+
+---11. List all payments made in the year 2023.
+
+SELECT *
+FROM Payment
+WHERE YEAR(paymentDate) = 2023;
+
+---12. Retrieve customers who have not made any payments.
+SELECT Customer.*
+FROM Customer
+LEFT JOIN Payment ON Customer.customerID = Payment.paymentID
+WHERE Payment.paymentID IS NULL;
+
+---13. 3. Retrieve Car Details and Their Total Payments
+
+SELECT
+    Vehicle.*,
+    COALESCE(totalPayments, 0) AS totalPayments
+FROM
+    Vehicle
+LEFT JOIN (
+    SELECT
+        Lease.vehicleID,
+        SUM(Payment.amount) AS totalPayments
+    FROM
+        Lease
+    LEFT JOIN
+        Payment ON Lease.leaseID = Payment.leaseID
+    GROUP BY
+        Lease.vehicleID
+) AS PaymentSummary ON Vehicle.vehicleID = PaymentSummary.vehicleID;
+
+---14. Calculate Total Payments for Each Customer
+SELECT
+    Customer.*,
+    COALESCE(
+        (SELECT SUM(Payment.amount)
+         FROM Lease
+         LEFT JOIN Payment ON Lease.leaseID = Payment.leaseID
+         WHERE Lease.customerID = Customer.customerID),
+        0
+    ) AS totalPayments
+FROM
+    Customer;
+
+---15. List Car Details for Each Lease.
+
+SELECT
+    Lease.leaseID,
+    Lease.startDate,
+    Lease.endDate,
+    Vehicle.vehicleID,
+    Vehicle.make,
+    Vehicle.model
+FROM
+    Lease
+JOIN
+    Vehicle ON Lease.vehicleID = Vehicle.vehicleID;
+
+
+---16. Retrieve Details of Active Leases with Customer and Car Information
+
+SELECT
+    Lease.leaseID,
+    Lease.startDate,
+    Lease.endDate,
+    Customer.customerID,
+    Customer.firstName,
+    Customer.lastName,
+    Vehicle.vehicleID,
+    Vehicle.make,
+    Vehicle.model
+FROM
+    Lease
+JOIN
+    Customer ON Lease.customerID = Customer.customerID
+JOIN
+    Vehicle ON Lease.vehicleID = Vehicle.vehicleID
+WHERE
+    '2023-11-28' BETWEEN Lease.startDate AND Lease.endDate;
+
+---17. Find the Customer Who Has Spent the Most on Leases.SELECT
+    customerID,
+    firstName,
+    lastName,
+    totalSpentOnLeases
+FROM (
+    SELECT
+        Customer.customerID,
+        Customer.firstName,
+        Customer.lastName,
+        COALESCE(SUM(Payment.amount), 0) AS totalSpentOnLeases,
+        RANK() OVER (ORDER BY COALESCE(SUM(Payment.amount), 0) DESC) AS rnk
+    FROM
+        Customer
+    LEFT JOIN
+        Lease ON Customer.customerID = Lease.customerID
+    LEFT JOIN
+        Payment ON Lease.leaseID = Payment.leaseID
+    GROUP BY
+        Customer.customerID, Customer.firstName, Customer.lastName
+) ranked
+WHERE
+    rnk = 1;
+
+--- 18. List All Cars with Their Current Lease Information
+
+	SELECT Vehicle.*, Lease.startDate, Lease.endDate, Customer.firstName, Customer.lastName
+FROM Vehicle
+LEFT JOIN Lease ON Vehicle.vehicleID = Lease.vehicleID
+LEFT JOIN Customer ON Lease.customerID = Customer.customerID
+
+
+
+
+
+
+
+
+
+
+
---
+++ CODING CHALLENGE/CODING-CHALLENGE-ECOM.sql
@@ -0,0 +1,274 @@
+create database ECOM;
+use ECOM;
+
+
+
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    Address VARCHAR(255)
+);
+
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    name VARCHAR(255),
+    price DECIMAL(10, 2),
+    description TEXT,
+    stockQuantity INT
+);
+
+CREATE TABLE cart (
+    cart_id INT PRIMARY KEY,
+    customer_id INT,
+    product_id INT,
+    quantity INT,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_price DECIMAL(10, 2),
+    shipping_address TEXT,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE order_items (
+    order_item_id INT PRIMARY KEY,
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+
+
+INSERT INTO customers (customer_id, name, email, Address)
+VALUES(1, 'John Doe', 'johndoe@example.com', '123 Main St, City'),
+(2, 'Jane Smith', 'janesmith@example.com', '456 Elm St, Town'),
+(3, 'Robert Johnson', 'robert@example.com', '789 Oak St, Village'),
+(4, 'Sarah Brown', 'sarah@example.com', '101 Pine St, Suburb'),
+(5, 'David Lee', 'david@example.com', '234 Cedar St, District'),
+(6, 'Laura Hall', 'laura@example.com', '567 Birch St, County'),
+(7, 'Michael Davis', 'michael@example.com', '890 Maple St, State'),
+(8, 'Emma Wilson', 'emma@example.com', '321 Redwood St, Country'),
+(9, 'William Taylor', 'william@example.com', '432 Spruce St, Province'),
+(10, 'Olivia Adams', 'olivia@example.com', '765 Fir St, Territory');
+
+select * from customers;
+
+
+
+INSERT INTO products (product_id, name, price, description, stockQuantity)
+VALUES(1, 'Laptop', 800.00, 'High-performance laptop', 10),
+(2, 'Smartphone', 600.00, 'Latest smartphone', 15),
+(3, 'Tablet', 300.00, 'Portable tablet', 20),
+(4, 'Headphones', 150.00, 'Noise-canceling', 30),
+(5, 'TV', 900.00, '4K Smart TV', 5),
+(6, 'Coffee Maker', 50.00, 'Automatic coffee maker', 25),
+(7, 'Refrigerator', 700.00, 'Energy-efficient', 10),
+(8, 'Microwave Oven', 80.00, 'Countertop microwave', 15),
+(9, 'Blender', 70.00, 'High-speed blender', 20),
+(10, 'Vacuum Cleaner', 120.00, 'Bagless vacuum cleaner', 10);
+
+select * from products;
+
+INSERT INTO cart 
+VALUES(1, 1, 1, 2),
+(2, 1, 3, 1),
+(3, 2, 2, 3),
+(4, 3, 4, 4),
+(5, 3, 5, 2),
+(6, 4, 6, 1),
+(7, 5, 1, 1),
+(8, 6, 10, 2),
+(9, 6, 9, 3),
+(10, 7, 7, 2);
+
+select * from cart;
+
+
+INSERT INTO orders (order_id, customer_id, order_date, total_price)
+VALUES (1, 1, '2023-01-05', 1200.00),
+(2, 2, '2023-02-10', 900.00),
+(3, 3, '2023-03-15', 300.00),
+(4, 4, '2023-04-20', 150.00),
+(5, 5, '2023-05-25', 1800.00),
+(6, 6, '2023-06-30', 400.00),
+(7, 7, '2023-07-05', 700.00),
+(8, 8, '2023-08-10', 160.00),
+(9, 9, '2023-09-15', 140.00),
+(10, 10, '2023-10-20', 1400.00);
+
+select * from orders;
+
+alter table order_items
+add item_amount decimal (10,2);
+
+-- Inserting values into the order_items table
+INSERT INTO order_items (order_item_id, order_id, product_id, quantity, item_amount)
+VALUES(1, 1, 1, 2, 1600.00),
+(2, 2, 3, 1, 300.00),
+(3, 3, 2, 3, 1800.00),
+(4, 4, 5, 2, 1800.00),
+(5, 4, 4, 4, 600.00),
+(6, 4, 6, 1, 50.00),
+(7, 5, 1, 1, 800.00),
+(8, 5, 2, 2, 1200.00),
+(9, 6, 10, 2, 240.00),
+(10, 6, 9, 3, 210.00);
+
+select * from order_items;
+
+--- 1
+UPDATE products
+SET price = 800.00
+WHERE product_id = 7;
+
+---2
+DELETE FROM cart
+WHERE customer_id = 5;
+
+---3
+SELECT *FROM products
+WHERE price <= 100.00;
+
+---4
+SELECT *FROM products
+WHERE stockQuantity > 5;
+
+---5
+SELECT *FROM orders
+WHERE total_price BETWEEN 500.00 AND 1000.00;
+
+---6
+SELECT *FROM products
+WHERE name LIKE '%r';
+
+---7
+SELECT *FROM cart
+WHERE customer_id = 5;
+
+---8
+SELECT DISTINCT c.*FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+WHERE YEAR(o.order_date) = 2023;
+
+---9
+SELECT product_id, name, MIN(stockQuantity) AS min_stock
+FROM products
+GROUP BY product_id, name;
+
+---10
+select customer_id, total_price from orders;
+
+---11
+
+SELECT
+    c.customer_id,
+    c.name AS customer_name,
+    AVG(o.total_price) AS average_order_amount
+FROM
+    customers c
+JOIN
+    orders o ON c.customer_id = o.customer_id
+GROUP BY
+    c.customer_id, c.name;
+
+---12
+
+select customer_id, quantity from cart;
+
+
+---13
+
+SELECT
+    c.customer_id,
+    c.name AS customer_name,
+    MAX(o.total_price) AS max_order_amount
+FROM
+    customers c
+JOIN
+    orders o ON c.customer_id = o.customer_id
+GROUP BY
+    c.customer_id, c.name;
+
+---14
+
+SELECT
+    c.customer_id,
+    c.name AS customer_name
+FROM
+    customers c
+JOIN
+    orders o ON c.customer_id = o.customer_id
+GROUP BY
+    c.customer_id, c.name
+HAVING
+    SUM(o.total_price) > 1000;
+
+---15
+
+-- Subquery to find products not in the cart
+SELECT *
+FROM products
+WHERE product_id NOT IN (
+    SELECT product_id
+    FROM cart
+);
+
+---16
+
+SELECT *
+FROM customers
+WHERE customer_id NOT IN (
+    SELECT DISTINCT customer_id
+    FROM orders
+);
+
+---17
+
+SELECT products.product_id, products.name,
+SUM(order_items.item_amount) AS total_revenue,
+ROUND((SUM(order_items.item_amount) / (SELECT SUM(orders.total_price) FROM orders)) * 100, 2) AS revenue_percentage
+FROM products
+JOIN order_items ON products.product_id = order_items.product_id
+JOIN orders ON order_items.order_id = orders.order_id
+GROUP BY products.product_id, products.name;
+
+
+---18
+
+SELECT *FROM products
+WHERE stockQuantity <= 5;
+
+---19
+SELECT *FROM customers
+WHERE customer_id IN (SELECT customer_id FROM orders WHERE total_price > 1000);
+
+---20
+
+SELECT
+    c.customer_id,
+    c.name AS customer_name
+FROM
+    customers c
+WHERE
+    c.customer_id IN (
+        SELECT DISTINCT
+            o.customer_id
+        FROM
+            orders o
+        JOIN
+            order_items oi ON o.order_id = oi.order_id
+        WHERE
+            oi.item_amount > 500 
+    );
+
+
+
+
